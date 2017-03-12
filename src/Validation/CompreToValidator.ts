b750572976d272243873230092bceb8e987f0ddc
Максим Курдюков < >
2017-03-12 12:35:14 +0400

    added dictionary

--- src/Validation/CompreToValidator.ts
+++ src/Validation/CompreToValidator.ts
@@ -1,7 +1,16 @@
 import IValidator from "./IValidator"
+import Dictionary from "../Tools/Dictionary";
 
 abstract class CompareToValidator implements IValidator{
-    constructor(protected otherId: string){
+    static rules: Dictionary<CompareRule, (first: number | string, second: number | string) => boolean>;
+    static init(): void {
+        CompareToValidator.rules = new Dictionary<CompareRule, (first: number | string, second: number | string) => boolean>();
+        CompareToValidator.rules.add(CompareRule.GreatThanOrEqualTo, ())
+    }
+    constructor(    protected otherValue: string,
+                    protected rule: CompareRule, 
+                    protected memberType: string, 
+                    protected isEnum: boolean){
     }
 
     isValid(value: any): boolean{
@@ -14,5 +23,23 @@ abstract class CompareToValidator implements IValidator{
 
     abstract compareValues(value: any, other: any) : boolean;
 }
+
+interface ICompareTo
+{
+
+}
+
+class CompareToNumbers implements ICompareTo {
+
+}
+CompareToValidator.init();
+
+export enum CompareRule{
+    GreaterThan,
+    GreatThanOrEqualTo,
+    LessThan,
+    LessThanOrEqualTo,
+    NotEqualTo
+};
 
 export default CompareToValidator;
